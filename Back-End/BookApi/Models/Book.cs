275fef76bfbc97f31bb147a5bae69db5c5b99f9f
Naami Ahmed <naamiahmed27@gmail.com>
2024-12-12 12:02:25 +0530

    Refactor project structure by consolidating front-end and back-end components, adding initial files and configurations

---
+++ Back-End/BookApi/Models/Book.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
